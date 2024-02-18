19675906d3447f33154aa9b3a3efd8d2c55a4f15
Weronika Mazurczak <weronikamazurczak202@gmail.com>
2024-02-18 16:29:57 +0100

    Initialize project structure

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <View>
+        <Text>QuizOdyssey</Text>
+      </View>
+    </>
+  );
+}
+
