e7092c6b96134cb54f2c2ba6bbb8632222fb3bf9
Weronika Mazurczak <weronikamazurczak202@gmail.com>
2024-02-18 16:48:04 +0100

    Navigation project structure

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SplashScreen from "./src/SplashScreen/SplashScreen";
+import StartScreen from "./src/StartScreen/StartScreen";
+import DetailsQuestion from "./src/DetailsQuestion/DetailsQuestion";
+import QuestionScreen from "./src/QuestionScreen/QuestionScreen";
+import FinishQuiz from "./src/FinishQuiz/FinishQuiz";
+import React from "react";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <View>
-        <Text>QuizOdyssey</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="DetailsQuestion" component={DetailsQuestion} />
+          <Stack.Screen name="QuestionScreen" component={QuestionScreen} />
+          <Stack.Screen name="FinishQuiz" component={FinishQuiz} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
-
