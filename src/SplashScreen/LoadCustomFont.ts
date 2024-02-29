a78a152af14bd12c13db1cef93857dd3706bd518
Weronika Mazurczak <weronikamazurczak202@gmail.com>
2024-02-29 14:20:47 +0100

    Update fonts

--- src/SplashScreen/LoadCustomFont.ts
+++ src/SplashScreen/LoadCustomFont.ts
@@ -4,6 +4,8 @@ export const loadCustomFont = async (): Promise<boolean> => {
   try {
     await Font.loadAsync({
       "KeaniaOne-Regular": require("../../assets/font/KeaniaOne-Regular.ttf"),
+      "QuartzoBold-W9lv": require("../../assets/font/QuartzoBold-W9lv.ttf"),
+      "libre-bodoni.regular": require("../../assets/font/libre-bodoni.regular.ttf"),
     });
     return true;
   } catch (exception) {
